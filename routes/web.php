6154a26f4d3f9e5ae9c338e6c757d16016ebf52b
Aang Pangantyas Sampurna <aangpangantyas@gmail.com>
2022-05-02 09:36:08 +0800

    Index routes, views, controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FrontendController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [FrontendController::class, 'index']);
 
 Route::middleware([
     'auth:sanctum',
