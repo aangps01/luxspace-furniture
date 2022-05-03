d22256bbf2f533376201113773f548dbada445ed
Aang Pangantyas Sampurna <aangpangantyas@gmail.com>
2022-05-03 20:23:35 +0800

    fix color black navbar for cart

--- resources/views/components/frontend/navbar.blade.php
+++ resources/views/components/frontend/navbar.blade.php
@@ -11,16 +11,20 @@
                 <ul class="fixed bg-white inset-0 flex flex-col invisible items-center justify-center opacity-0 md:visible md:flex-row md:bg-transparent md:relative md:opacity-100 md:flex md:items-center"
                     id="menu">
                     <li class="mx-3 py-6 md:py-0">
-                        <a href="#" class="text-black md:text-white hover:underline">Showcase</a>
+                        <a href="#"
+                            class="text-black {{ (Request::is('cart') ? 'md:text-black' : 'md:text-white') }} hover:underline">Showcase</a>
                     </li>
                     <li class="mx-3 py-6 md:py-0">
-                        <a href="#" class="text-black md:text-white hover:underline">Catalog</a>
+                        <a href="#"
+                            class="text-black {{ (Request::is('cart') ? 'md:text-black' : 'md:text-white') }} hover:underline">Catalog</a>
                     </li>
                     <li class="mx-3 py-6 md:py-0">
-                        <a href="#" class="text-black md:text-white hover:underline">Delivery</a>
+                        <a href="#"
+                            class="text-black {{ (Request::is('cart') ? 'md:text-black' : 'md:text-white') }} hover:underline">Delivery</a>
                     </li>
                     <li class="mx-3 py-6 md:py-0">
-                        <a href="#" class="text-black md:text-white hover:underline">Rewards</a>
+                        <a href="#"
+                            class="text-black {{ (Request::is('cart') ? 'md:text-black' : 'md:text-white') }} hover:underline">Rewards</a>
                     </li>
                 </ul>
             </div>
