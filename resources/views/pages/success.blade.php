eb145ae1f796f30ded8e65a39ae96db2f05991db
Aang Pangantyas Sampurna <aangpangantyas@gmail.com>
2022-05-04 02:47:36 +0800

    change logo dashboard

--- resources/views/pages/success.blade.php
+++ resources/views/pages/success.blade.php
@@ -28,7 +28,7 @@
                     Furniture yang anda beli akan kami kirimkan saat ini juga so now
                     please sit tight and be ready for it
                 </p>
-                <a href="route('details')"
+                <a href="{{ url()->previous() }}"
                     class="text-gray-900 bg-red-200 focus:outline-none w-full py-3 rounded-full text-lg focus:text-black transition-all duration-200 px-8 cursor-pointer">
                     Back to Shop
                 </a>
