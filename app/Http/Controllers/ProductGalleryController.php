91d94ff9c64ace980c1c15e9d48d709e8afe1ccf
Aang Pangantyas Sampurna <aangpangantyas@gmail.com>
2022-05-07 01:42:08 +0700

    checkpoint add toggle add style toggle

--- app/Http/Controllers/ProductGalleryController.php
+++ app/Http/Controllers/ProductGalleryController.php
@@ -33,7 +33,13 @@ class ProductGalleryController extends Controller
                     return '<img src="' . Storage::url($data->url) . '" style="max-width: 180px">';
                 })
                 ->editColumn('is_featured', function ($data) {
-                    return $data->is_featured ? 'Yes' : 'No';
+                    return '
+                        <label for="default-toggle" class="inline-flex relative items-center cursor-pointer">
+                        <input type="checkbox" value="" id="default-toggle" class="sr-only peer">
+                        <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
+                        <span class="ml-3 text-sm font-medium text-gray-900 ">Toggle me</span>
+                        </label>
+                    ';
                 })
                 ->rawColumns(['action'])
                 ->make(true);
